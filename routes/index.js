de83472e6169289dce75bd4e16eb37d65d6c9234
davidh <iq.davidh@gmail.com>
2019-07-13 14:24:26 -0500

    ya tenemos la vista

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,50 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+	res.render('index');
 });
 
 
 /* con authenticacion ********************************************************* */
 
-const ensureAuthenticated =(req, res, next) =>{
-  if (req.isAuthenticated()) {
-    console.log('current user');
-    console.warn( req.user);
+const ensureAuthenticated = (req, res, next) => {
+	if (req.isAuthenticated()) {
+		console.log('current user');
+		console.warn(req.user);
 
-    return next();
-  } else {
-    res.redirect('/login')
-  }
+		return next();
+	} else {
+		res.redirect('/login')
+	}
+};
+
+const checkRoles = (role) => {
+	return function (req, res, next) {
+
+		if (!req.isAuthenticated()) {
+			res.redirect('/login')
+		}
+
+		if (req.user.role === 'ADMIN') {
+			return next();
+		} else if (req.user.role === role) {
+			return next();
+		} else {
+			res.redirect('/login')
+		}
+
+	}
 };
 
 router.get("/private-page", ensureAuthenticated, (req, res) => {
-  res.render("private", { user: req.user });
+	res.render("private", {user: req.user});
+});
+
+
+router.get('/private-only-admin', checkRoles('ADMIN'), (req, res) => {
+	res.render('private-only-admin', {user: req.user});
 });
 
 
